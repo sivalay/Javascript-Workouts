b97b9e40513b58511cf279a89e8cde4edb87e567
Siva <sivalayavinod25@gmail.com>
2025-07-08 07:16:10 +0530

    function to swap zero's

--- July-WorkOuts.js
+++ July-WorkOuts.js
@@ -4,3 +4,23 @@ function convertToArr(arr) {
   let c = arr.flat(Infinity);
   console.log(...c);
 }
+convertToArr(theValues);
+
+// Convert the Zeros to the end
+
+const testList = [2, 4, 7, 0, 21, 3, 0, 3, 0];
+
+function swapZeroToEnd(arr) {
+  let string = '';
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] == 0) {
+      arr.push(arr[i]);
+      arr.splice(i, 1);
+    }
+    string += arr[i];
+  }
+  console.log(string);
+}
+
+swapZeroToEnd(testList);
