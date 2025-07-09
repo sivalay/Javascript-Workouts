3da8c59f2d2050c9de11c3a9b76ff235a68546d4
Siva <sivalayavinod25@gmail.com>
2025-07-09 08:44:49 +0530

    convert zero to end from number and positive number count

--- July-WorkOuts.js
+++ July-WorkOuts.js
@@ -4,7 +4,7 @@ function convertToArr(arr) {
   let c = arr.flat(Infinity);
   console.log(...c);
 }
-convertToArr(theValues);
+// convertToArr(theValues);
 
 // Convert the Zeros to the end
 
@@ -22,5 +22,40 @@ function swapZeroToEnd(arr) {
   }
   console.log(string);
 }
+// swapZeroToEnd(testList);
 
-swapZeroToEnd(testList);
+// Program to convert Zero to the end of a string
+
+// const testString = [2, 4, 7, 0, 21, 3, 0, 3, 0];
+const testString = 24702130320;
+
+function convertZeroToEnd(arr) {
+  let string = '';
+  const stringToString = arr.toString();
+  const array = Array.from(stringToString);
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] == 0) {
+      array.push(array[i]);
+      array.splice(i, 1);
+    }
+  }
+  for (let i = 0; i < array.length; i++) {
+    string += array[i];
+  }
+  console.log(string);
+}
+
+convertZeroToEnd(testString);
+
+// to count the positive numbers
+arr = [1, -2, 3, 4];
+function countPositiveNumbers(arr) {
+  count = 0;
+  arr.forEach((value) => {
+    if (value >= 0) {
+      count++;
+    }
+  });
+  console.log(count);
+}
+countPositiveNumbers(arr);
